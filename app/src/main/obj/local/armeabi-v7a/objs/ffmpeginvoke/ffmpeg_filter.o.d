830e216b4c7b78ece963b2bc4857132c8e9a8363
Sai <zhouquanbest@gmail.com>
2017-05-13 20:01:56 +0800

    update

---
+++ app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/ffmpeg_filter.o.d
@@ -0,0 +1,140 @@
+/Users/kleist/code/test/MyFFmpeg/app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/ffmpeg_filter.o: \
+  /Users/kleist/code/test/MyFFmpeg/app/src/main/jni/ffmpeg_filter.c \
+  /Users/kleist/code/test/MyFFmpeg/app/src/main/jni/ffmpeg.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/config.h \
+  /Users/kleist/code/test/MyFFmpeg/app/src/main/jni/cmdutils.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavcodec/avcodec.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/samplefmt.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avutil.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/common.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/attributes.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/macros.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avconfig.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/mem.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/error.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/rational.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/mathematics.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/intfloat.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/log.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/pixfmt.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/buffer.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/cpu.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/channel_layout.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/dict.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/frame.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavcodec/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/avfilter.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/avformat.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/avio.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libswscale/swscale.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libswscale/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/eval.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/fifo.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/threadmessage.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libswresample/swresample.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libswresample/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/buffersink.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavresample/avresample.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavresample/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avassert.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avstring.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/bprint.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/display.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/opt.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/pixdesc.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/imgutils.h
+
+/Users/kleist/code/test/MyFFmpeg/app/src/main/jni/ffmpeg.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/config.h:
+
+/Users/kleist/code/test/MyFFmpeg/app/src/main/jni/cmdutils.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavcodec/avcodec.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/samplefmt.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avutil.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/common.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/attributes.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/macros.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avconfig.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/mem.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/error.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/rational.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/mathematics.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/intfloat.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/log.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/pixfmt.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/buffer.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/cpu.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/channel_layout.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/dict.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/frame.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavcodec/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/avfilter.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/avformat.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/avio.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libswscale/swscale.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libswscale/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/eval.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/fifo.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/threadmessage.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libswresample/swresample.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libswresample/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/buffersink.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavresample/avresample.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavresample/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avassert.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avstring.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/bprint.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/display.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/opt.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/pixdesc.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/imgutils.h:
---
+++ app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/ffmpeg_opt.o.d
@@ -0,0 +1,137 @@
+/Users/kleist/code/test/MyFFmpeg/app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/ffmpeg_opt.o: \
+  /Users/kleist/code/test/MyFFmpeg/app/src/main/jni/ffmpeg_opt.c \
+  /Users/kleist/code/test/MyFFmpeg/app/src/main/jni/ffmpeg.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/config.h \
+  /Users/kleist/code/test/MyFFmpeg/app/src/main/jni/cmdutils.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavcodec/avcodec.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/samplefmt.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avutil.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/common.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/attributes.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/macros.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avconfig.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/mem.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/error.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/rational.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/mathematics.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/intfloat.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/log.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/pixfmt.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/buffer.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/cpu.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/channel_layout.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/dict.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/frame.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavcodec/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/avfilter.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/avformat.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/avio.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libswscale/swscale.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libswscale/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/eval.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/fifo.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/threadmessage.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libswresample/swresample.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libswresample/version.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avassert.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avstring.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/intreadwrite.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/bswap.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/opt.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/parseutils.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/pixdesc.h \
+  /Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/time_internal.h \
+  /Users/kleist/code/test/MyFFmpeg/app/src/main/jni/cmdutils_common_opts.h
+
+/Users/kleist/code/test/MyFFmpeg/app/src/main/jni/ffmpeg.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/config.h:
+
+/Users/kleist/code/test/MyFFmpeg/app/src/main/jni/cmdutils.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavcodec/avcodec.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/samplefmt.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avutil.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/common.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/attributes.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/macros.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avconfig.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/mem.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/error.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/rational.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/mathematics.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/intfloat.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/log.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/pixfmt.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/buffer.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/cpu.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/channel_layout.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/dict.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/frame.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavcodec/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/avfilter.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavfilter/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/avformat.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/avio.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavformat/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libswscale/swscale.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libswscale/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/eval.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/fifo.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/threadmessage.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libswresample/swresample.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libswresample/version.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avassert.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/avstring.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/intreadwrite.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/bswap.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/opt.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/parseutils.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/pixdesc.h:
+
+/Users/kleist/Downloads/ffmpeg-3.0.7/libavutil/time_internal.h:
+
+/Users/kleist/code/test/MyFFmpeg/app/src/main/jni/cmdutils_common_opts.h:
